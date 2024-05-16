577a78aea7c055911241c9a39754a9623a7ea0e5
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2024-05-16 13:34:33 +0000

    save change

--- bash_scripts_nips/sample_compt_glide/run.sh
+++ bash_scripts_nips/sample_compt_glide/run.sh
@@ -2,9 +2,9 @@
 
 
 
-#HFAI_DATASETS_DIR=/hdd/datasets/ hfai bash bash_scripts_nips/sample_compt_glide/IM64_compt.sh  ++
+HFAI_DATASETS_DIR=/hdd/datasets/ hfai bash bash_scripts_nips/sample_compt_glide/IM64_compt.sh  ++
 
 
-HFAI_DATASETS_DIR=/hdd/datasets/ hfai bash bash_scripts_nips/sample_compt_glide/IM256_compt.sh  ++
+#HFAI_DATASETS_DIR=/hdd/datasets/ hfai bash bash_scripts_nips/sample_compt_glide/IM256_compt.sh  ++
 #HFAI_DATASETS_DIR=/hdd/datasets/ hfai bash bash_scripts_nips/sample_compt_glide/IM256_comptquad.sh  ++
 
