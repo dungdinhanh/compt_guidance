1997d4f4f811bba2a4892a22ff46b542b3ac007f
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2024-09-06 09:39:29 +1000

    finish coding auto s

---
+++ scripts_gdiff/compt_guidance/classifier_compt_sample_general_autos.py
@@ -0,0 +1,267 @@
+"""
+Like image_sample.py, but use a noisy image classifier to guide the sampling
+process towards more realistic images.
+"""
+
+import argparse
+import os
+
+import numpy as np
+import torch as th
+# import torch.distributed as dist
+import hfai.nccl.distributed as dist
+import torch.nn.functional as F
+import hfai
+from guided_diffusion import dist_util, logger
+from guided_diffusion.script_util import (
+    NUM_CLASSES,
+    model_and_diffusion_defaults,
+    classifier_defaults,
+    create_model_and_diffusion,
+    create_classifier,
+    add_dict_to_argparser,
+    args_to_dict,
+)
+import datetime
+from PIL import Image
+# import hfai.client
+from torchvision import utils
+
+
+
+def main(local_rank):
+    args = create_argparser().parse_args()
+
+    dist_util.setup_dist(local_rank)
+
+    if args.fix_seed:
+        import random
+        seed = 23333 + dist.get_rank()
+        np.random.seed(seed)
+        th.manual_seed(seed)  # CPU随机种子确定
+        th.cuda.manual_seed(seed)  # GPU随机种子确定
+        th.cuda.manual_seed_all(seed)  # 所有的GPU设置种子
+
+        th.backends.cudnn.benchmark = False  # 模型卷积层预先优化关闭
+        th.backends.cudnn.deterministic = True  # 确定为默认卷积算法
+
+        random.seed(seed)
+        np.random.seed(seed)
+
+        os.environ['PYTHONHASHSEED'] = str(seed)
+    base_folder = args.base_folder
+    save_folder = os.path.join(
+        base_folder,
+        args.logdir,
+        "logs",
+    )
+
+    logger.configure(save_folder, rank=dist.get_rank())
+
+    output_images_folder = os.path.join(base_folder, args.logdir, "reference")
+    os.makedirs(output_images_folder, exist_ok=True)
+
+    logger.log("creating model and diffusion...")
+    model, diffusion = create_model_and_diffusion(
+        **args_to_dict(args, model_and_diffusion_defaults().keys())
+    )
+    model.load_state_dict(
+        dist_util.load_state_dict(os.path.join(base_folder, args.model_path), map_location="cpu")
+    )
+    model.to(dist_util.dev())
+    if args.use_fp16:
+        model.convert_to_fp16()
+    model.eval()
+
+
+    logger.log("loading classifier...")
+    classifier = create_classifier(**args_to_dict(args, classifier_defaults().keys()))
+    classifier.load_state_dict(
+        dist_util.load_state_dict(os.path.join(base_folder, args.classifier_path), map_location="cpu")
+    )
+    classifier.to(dist_util.dev())
+    if args.classifier_use_fp16:
+        classifier.convert_to_fp16()
+    classifier.eval()
+    df_steps = int(args.diffusion_steps)
+    timespace = int(args.timestep_respacing)
+    steps_skipped_diff = int(df_steps/timespace)
+    skip = int(args.skip)
+
+    guidance_timesteps, compression_steps = get_guidance_timesteps_with_weight(timespace, skip, args.k)
+
+    def cond_fn(x, t, y=None):
+        assert y is not None
+        with th.enable_grad():
+            convert_t = int(t[0]/steps_skipped_diff)
+            x_in = x.detach().requires_grad_(True)
+            if guidance_timesteps[convert_t] > 0:
+                # print("call guidance:-------------->", convert_t)
+                logits = classifier(x_in, t)
+                log_probs = F.log_softmax(logits, dim=-1)
+                selected = log_probs[range(len(logits)), y.view(-1)]
+                
+                check_scale = args.classifier_scale * guidance_timesteps[convert_t] * compression_steps[convert_t]
+                print(f"{t[0]}, {check_scale}")
+                return th.autograd.grad(selected.sum(), x_in)[0] * args.classifier_scale * guidance_timesteps[convert_t] * compression_steps[convert_t]
+            else:
+                # print("skip: ", convert_t)
+                return th.zeros_like(x_in)
+
+    def model_fn(x, t, y=None):
+        assert y is not None
+        return model(x, t, y if args.class_cond else None)
+
+    logger.log("Looking for previous file")
+    checkpoint = os.path.join(output_images_folder, "samples_last.npz")
+    vis_images_folder = os.path.join(output_images_folder, "sample_images")
+    os.makedirs(vis_images_folder, exist_ok=True)
+    final_file = os.path.join(output_images_folder,
+                              f"samples_{args.num_samples}x{args.image_size}x{args.image_size}x3.npz")
+    if os.path.isfile(final_file):
+        dist.barrier()
+        logger.log("sampling complete")
+        return
+    if os.path.isfile(checkpoint):
+        npzfile = np.load(checkpoint)
+        all_images = list(npzfile['arr_0'])
+        all_labels = list(npzfile['arr_1'])
+    else:
+        all_images = []
+        all_labels = []
+    logger.log(f"Number of current images: {len(all_images)}")
+    logger.log("sampling...")
+    if args.image_size == 28:
+        img_channels = 1
+        num_class = 10
+    else:
+        img_channels = 3
+        num_class = NUM_CLASSES
+    while len(all_images) * args.batch_size < args.num_samples:
+        model_kwargs = {}
+        classes = th.randint(
+            low=0, high=num_class, size=(args.batch_size,), device=dist_util.dev()
+        )
+        model_kwargs["y"] = classes
+        sample_fn = (
+            diffusion.p_sample_loop if not args.use_ddim else diffusion.ddim_sample_loop
+        )
+        sample = sample_fn(
+            model_fn,
+            (args.batch_size, img_channels, args.image_size, args.image_size),
+            clip_denoised=args.clip_denoised,
+            model_kwargs=model_kwargs,
+            cond_fn=cond_fn,
+            device=dist_util.dev(),
+        )
+
+        if args.save_imgs_for_visualization and dist.get_rank() == 0 and (
+                len(all_images) // dist.get_world_size()) < 10:
+            save_img_dir = vis_images_folder
+            utils.save_image(
+                sample.clamp(-1, 1),
+                os.path.join(save_img_dir, "samples_{}.png".format(len(all_images))),
+                nrow=4,
+                normalize=True,
+                range=(-1, 1),
+            )
+
+        sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
+        sample = sample.permute(0, 2, 3, 1)
+        sample = sample.contiguous()
+
+        gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
+        dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
+        batch_images = [sample.cpu().numpy() for sample in gathered_samples]
+        all_images.extend(batch_images)
+        gathered_labels = [th.zeros_like(classes) for _ in range(dist.get_world_size())]
+        dist.all_gather(gathered_labels, classes)
+        batch_labels = [labels.cpu().numpy() for labels in gathered_labels]
+        all_labels.extend(batch_labels)
+        if dist.get_rank() == 0:
+            if hfai.client.receive_suspend_command():
+                print("Receive suspend - good luck next run ^^")
+                hfai.client.go_suspend()
+            logger.log(f"created {len(all_images) * args.batch_size} samples")
+            np.savez(checkpoint, np.stack(all_images), np.stack(all_labels))
+
+    arr = np.concatenate(all_images, axis=0)
+    arr = arr[: args.num_samples]
+    label_arr = np.concatenate(all_labels, axis=0)
+    label_arr = label_arr[: args.num_samples]
+    if dist.get_rank() == 0:
+        shape_str = "x".join([str(x) for x in arr.shape])
+        out_path = os.path.join(output_images_folder, f"samples_{shape_str}.npz")
+        logger.log(f"saving to {out_path}")
+        np.savez(out_path, arr, label_arr)
+        os.remove(checkpoint)
+
+    dist.barrier()
+    logger.log("sampling complete")
+
+def get_guidance_timesteps_with_weight(n=250, skip=5, k=2.0):
+    # c * i^2
+    T = n - 1
+    max_steps = int(n/skip)
+    c = n/(max_steps**k)
+    guidance_timesteps = np.zeros((n,), dtype=int)
+    for i in range(max_steps):
+        guidance_index = - int(c * (i ** k)) + T
+        if 0 <= guidance_index and guidance_index <= T:
+            guidance_timesteps[guidance_index] = 1
+        else:
+            print(f"guidance index: {guidance_index}")
+            print(f"constant c: {c}")
+            print(f"faulty index: {i}")
+            print(f"timesteps {T}")
+            print(f"compressd by {skip} times")
+            print(f"error in index must larger than 0 or less than {T}")
+            exit(0)
+    guidance_timesteps[1] = 1
+    guidance_timesteps[0] = 1
+    compression_up = np.zeros_like(guidance_timesteps)
+    # cur_index = 0
+    accumulate = 0
+    for i in range(n):
+        if guidance_timesteps[i] != 0:
+            compression_up[i] = guidance_timesteps[i] + accumulate
+            accumulate = 0
+        else:
+            accumulate += 1
+        pass
+    # print(guidance_timesteps)
+    # print(np.sum(guidance_timesteps))
+    # print("__________________________________")
+    # print(compression_up)
+    # exit(0)
+    return guidance_timesteps, compression_up
+
+
+def create_argparser():
+    defaults = dict(
+        clip_denoised=True,
+        num_samples=50000,
+        batch_size=16,
+        use_ddim=False,
+        model_path="",
+        classifier_path="",
+        classifier_scale=1.0,
+        save_imgs_for_visualization=True,
+        fix_seed=False,
+        specified_class=None,
+        logdir="",
+        base_folder="./",
+        skip=5,
+        k=2.0
+    )
+    defaults.update(model_and_diffusion_defaults())
+    defaults.update(classifier_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+
+if __name__ == "__main__":
+    ngpus = th.cuda.device_count()
+    th.multiprocessing.spawn(main, args=(), nprocs=ngpus)
